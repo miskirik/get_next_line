48b77f1b7f767ec26c1746aa80f935b439933318
Muhammet <miskirik@server.local>
2022-06-26 01:18:34 +0300

    files changed

--- get_next_line.c
+++ get_next_line.c
@@ -6,6 +6,46 @@
 /*   By: miskirik <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 16:38:05 by miskirik          #+#    #+#             */
-/*   Updated: 2022/06/24 16:38:33 by miskirik         ###   ########.fr       */
+/*   Updated: 2022/06/26 01:13:59 by miskirik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "get_next_line.h"
+
+char	*ft_buff(int fd,char *str)
+{
+	int	rd_bytes;
+	char	*buff;
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if(!buff)
+		return(NULL);
+	rd_bytes = 1;
+	while(!ft_strchr(str, '\n') && rd_bytes != 0);
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(str);
+			free(buff);
+			return(NULL);
+		}
+		buff[rd_bytes] = '\0';
+		str = ft_strjoin(str, buff);
+	}
+	free(buff);
+	return(str);
+}
+char	*get_next_line(int fd)
+{
+	char				*line;
+	static char			*str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	str = ft_buff(fd, str);
+	if (!str)
+		return (NULL);
+	line = ft_get_line(str);
+	str = ft_last_str(str);
+	return (line);
+}
+
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,32 @@
 /*   By: miskirik <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 16:39:00 by miskirik          #+#    #+#             */
-/*   Updated: 2022/06/24 16:39:02 by miskirik         ###   ########.fr       */
+/*   Updated: 2022/06/26 01:17:48 by miskirik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+size_t ft_strlen(char *str)
+{
+	size_t	c;
 
+	c = 0;
+	if(!str)
+		return(0);
+	while(str[c])
+		c++;
+	return(c);
+}
+char	*ft_strjoin(char *str, char *buff)
+{
+	size_t	c;
+	size_t	i;
+	char	*str;
 
+	if(!str)
+	{
+		str = (char *)malloc(1 * sizeof(char));
+		str[0] = '\0';
+	}
+	if(!str || !buff)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen(str))))
